32ac14b7b1656f8ebcfa1b32b186c6f9a4ee0406
jpcirqueira <jpcirqueira81@gmail.com>
2021-04-23 18:42:35 -0300

    create component Button and Input.

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import Background from '~/components/Background';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 // import { Container } from './styles';
 
 const SignIn = () => {
   return (
   <Background>
     <Text>SignIn</Text>
+    <Input style={{marginTop:30}} icon="call" placeholder="digite seu telefone" />
+    <Button>entrar</Button>
   </Background>
   );
 }

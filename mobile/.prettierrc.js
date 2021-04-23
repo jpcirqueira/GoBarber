e0b9c912688d0482c9b0f6530891ff60dc433495
jpcirqueira <jpcirqueira81@gmail.com>
2021-04-23 15:37:07 -0300

    inital commit.

---
+++ mobile/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "singleQuote": true,
+  "trailingComma": "es5"
+  // bracketSpacing: false,
+  // jsxBracketSameLine: true,
+  // singleQuote: true,
+  // trailingComma: 'all',
+  // arrowParens: 'avoid',
+};

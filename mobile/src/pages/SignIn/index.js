9cd1073bf9392bcb0a79bdaab7e9d4881117b4bd
jpcirqueira <jpcirqueira81@gmail.com>
2021-04-24 15:40:21 -0300

    create page signIn.

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
 
-import Background from '~/components/Background';
-import Input from '~/components/Input';
-import Button from '~/components/Button';
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
+
+import Background from '~/components/Background'; 
+import { Container, Form, FormInput, SubmitButton, SignLinkText, SignLink } from './styles';
 
 const SignIn = () => {
   return (
   <Background>
-    <Text>SignIn</Text>
-    <Input style={{marginTop:30}} icon="call" placeholder="digite seu telefone" />
-    <Button>entrar</Button>
+    <Container>
+      <Image source={logo} />
+      <Form>
+        <FormInput 
+          icon="mail-outline"
+          keyboardType="email-address"
+          autoCorrect={false}
+          autoCapitalize="nome"
+          placeholder="Digite seu E-mail"
+        />
+        <FormInput 
+          icon="lock-outline"
+          secureTextEntry
+          placeholder="Sua senha secreta"
+        />
+        <SubmitButton OnPress={() => {}}>
+          Acessar
+        </SubmitButton>
+      </Form>
+      <SignLink OnPress={() => {}}>
+        <SignLinkText>Criar conta gratuita</SignLinkText> 
+      </SignLink>
+    </Container>
   </Background>
   );
 }
